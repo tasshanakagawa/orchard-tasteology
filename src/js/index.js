96dcab1a626d2c2023b60ec1edd2581847f15bc3
Tassha Nakagawa <nakagawatassha@gmail.com>
2025-09-11 20:10:29 +0800

    Initial commit and file setup

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+import '../styles/index.scss';
+
+console.log('Webpack + SCSS setup is working!');
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+
+  return {
+    entry: './src/js/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true, // clears dist before each build
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'sass-loader'
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'style.min.css',
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+    ],
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, 'dist'),
+      },
+      hot: true,
+      port: 8080,
+      open: true,
+    },
+    devtool: isProd ? false : 'source-map',
+    mode: isProd ? 'production' : 'development',
+  };
+};
