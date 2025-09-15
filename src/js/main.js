bbaaf25744d603c88c48ebce15f81aaf1c85682f
Tassha Nakagawa <nakagawatassha@gmail.com>
2025-09-15 23:07:34 +0800

    chore: update file structure

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+// Globals
+import '../styles/main.scss';
+
+// Components
+import { createGallery } from '../components/gallery/gallery.js';
+
+// Initialize components
+createGallery('.gallery');
--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,47 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = (env, argv) => {
-  const isProd = argv.mode === 'production';
+const devMode = process.env.NODE_ENV !== "production";
 
-  return {
-    entry: './src/js/index.js',
-    output: {
-      filename: 'main.js',
-      path: path.resolve(__dirname, 'dist'),
-      clean: true, // clears dist before each build
-    },
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          use: [
-            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
-            'css-loader',
-            'sass-loader'
-          ],
-        },
-      ],
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: 'main.css',
-      }),
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-      }),
-    ],
-    devServer: {
-      static: {
-        directory: path.resolve(__dirname, 'dist'),
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // clears dist before each build
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'main.css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
       },
-      hot: true,
-      port: 8080,
-      open: true,
-    },
-    devtool: isProd ? false : 'source-map',
-    mode: isProd ? 'production' : 'development',
-  };
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      }
+    ],
+  },
+  // devServer: {
+  //   static: {
+  //     directory: path.resolve(__dirname, 'dist'),
+  //   },
+  //   hot: true,
+  //   port: 8080,
+  //   open: true,
+  // },
+  // devtool: devMode ? 'source-map' : false,
+  // mode: devMode ? 'development' : 'production',
 };
