352ab12113c57c6f7cc0768066d8968278183499
Tassha Nakagawa <nakagawatassha@gmail.com>
2025-09-16 19:32:39 +0800

    feat(gallery): add modal function

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -1,6 +1,7 @@
 /**
  * @file gallery.js
- * @description Renders a gallery section with images and content
+ * @description Renders a gallery section with an image grid, content, and
+ * a modal on image click
  */
 
 // Import component styles
@@ -18,8 +19,8 @@ export function createGallery() {
     const src = require(`../../assets/gallery/${grid.image}`);
     
     return `
-      <div class="gallery__image">
-        <img src="${src}" alt="${grid.alt}">
+      <div class="gallery__image" role="button" tabindex="0">
+        <img src="${src}" alt="${grid.alt}" />
       </div>
     `;
   }).join('');
@@ -38,8 +39,52 @@ export function createGallery() {
         </div>
       </div>
     </div>
+
+    <!-- Modal -->
+    <div class="gallery__modal hidden" role="dialog" aria-modal="true" aria-label="Image Preview">
+      <div class="gallery__modal-wrapper">
+        <span class="gallery__modal-close" aria-label="Close modal">
+          <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="currentColor"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+        </span>
+        <div class="gallery__modal-image">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
   `;
 
   // Append the html to the container
   container.innerHTML = component;
+
+  // Add modal functionality
+  const gridItems = container.querySelectorAll('.gallery__image');
+  const modal = container.querySelector('.gallery__modal');
+  const modalImage = container.querySelector('.gallery__modal-image img');
+
+  gridItems.forEach(item => {
+    item.addEventListener('click', (e) => {
+      const image = item.querySelector('img');
+
+      modalImage.src = image.src;
+      modalImage.alt = image.alt;
+
+      modal.classList.remove('hidden');
+      document.body.style.overflow = 'hidden';
+    });
+  });
+
+  // Function to close modal
+  const closeButton = container.querySelector('.gallery__modal-close');
+  const closeModal = (e) => {
+    modal.classList.add('hidden');
+    document.body.style.overflow = '';
+  }
+
+  // Close modal on click and esc key
+  closeButton.addEventListener('click', closeModal);
+  closeButton.addEventListener('keyboard', (e) => {
+    if (!modal.classList.contains('hidden') && e.key === 'Escape') {
+      closeModal();
+    }
+  });
 }
