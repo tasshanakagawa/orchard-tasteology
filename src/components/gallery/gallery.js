9a59a0ef640445fb36005087679da9c0b9f54c21
Tassha Nakagawa <nakagawatassha@gmail.com>
2025-09-16 22:22:51 +0800

    feat(gallery): handle keyboard tabbing

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -43,9 +43,9 @@ export function createGallery() {
     <!-- Modal -->
     <div class="gallery__modal hidden" role="dialog" aria-modal="true" aria-label="Image Preview">
       <div class="gallery__modal-wrapper">
-        <span class="gallery__modal-close" aria-label="Close modal">
+        <button class="gallery__modal-close" aria-label="Close modal">
           <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="currentColor"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
-        </span>
+        </button>
         <div class="gallery__modal-image">
           <img src="" alt="" />
         </div>
@@ -56,33 +56,85 @@ export function createGallery() {
   // Append the html to the container
   container.innerHTML = component;
 
-  // Add modal functionality
+  // Modal
   const gridItems = container.querySelectorAll('.gallery__image');
   const modal = container.querySelector('.gallery__modal');
   const modalImage = container.querySelector('.gallery__modal-image img');
 
+  // Focus trapping
+  const focusableElements = modal.querySelectorAll('.gallery__modal-close');
+  const firstFocusable = focusableElements[0];
+  const lastFocusable = focusableElements[focusableElements.length - 1];
+  
+  const handleFocusTrap = (e) => {
+    if (e.shiftKey) {
+      // Shift + Tab
+      if (document.activeElement === firstFocusable) {
+        e.preventDefault();
+        lastFocusable.focus();
+      }
+    } else {
+      // Tab
+      if (document.activeElement === lastFocusable) {
+        e.preventDefault();
+        firstFocusable.focus();
+      }
+    }
+  };
+
+  // Open modal function
+  const openModal = (item) => {
+    const image = item.querySelector('img');
+
+    modalImage.src = image.src;
+    modalImage.alt = image.alt;
+
+    modal.classList.remove('hidden');
+    document.body.style.overflow = 'hidden';
+  }
+
+  // Open modal on click and enter
   gridItems.forEach(item => {
-    item.addEventListener('click', (e) => {
-      const image = item.querySelector('img');
+    item.addEventListener('click', () => openModal(item));
+    item.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter') {
+        openModal(item);
 
-      modalImage.src = image.src;
-      modalImage.alt = image.alt;
+        // Deselect focused element
+        document.activeElement.blur();
 
-      modal.classList.remove('hidden');
-      document.body.style.overflow = 'hidden';
+        // Switch focus to close button
+        setTimeout(() => firstFocusable.focus(), 100);
+
+        // Prevent keyboard tabbing outside of modal
+        modal.addEventListener('keydown', handleFocusTrap);
+      }
     });
   });
-
-  // Function to close modal
+ 
+  // Close modal function
   const closeButton = container.querySelector('.gallery__modal-close');
   const closeModal = (e) => {
     modal.classList.add('hidden');
     document.body.style.overflow = '';
+
+    // Allow keyboard tabbing
+    modal.removeEventListener('keydown', handleFocusTrap);
   }
 
-  // Close modal on click and esc key
+  // Close modal on click
   closeButton.addEventListener('click', closeModal);
-  closeButton.addEventListener('keyboard', (e) => {
+
+  // Close modal on keyboard enter
+  closeButton.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      closeModal();
+}
+  });
+
+  // Close modal on escape
+  document.addEventListener('keydown', (e) => {
     if (!modal.classList.contains('hidden') && e.key === 'Escape') {
       closeModal();
     }
