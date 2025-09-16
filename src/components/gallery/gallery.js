0eb02269753f53b615209df4518cd77153e05241
Tassha Nakagawa <nakagawatassha@gmail.com>
2025-09-16 16:28:56 +0800

    feat(gallery): get content from json file

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -7,25 +7,19 @@
 import './gallery.scss';
 
 // Import JSON data
-// import data from '../../data/gallery.json';
+import data from '../../data/gallery.json';
 
 // Create the component and inject to "gallery" container
 export function createGallery() {
   const container = document.querySelector('.gallery');
 
-  // Generate images
-  const images = [
-    { name: 'stove-pit.jpg', alt: 'Cooking on a stove pit' },
-    { name: 'chef.jpg', alt: 'Chef selecting cooking spices on a kitchen counter' },
-    { name: 'hard-boiled-eggs.jpg', alt: 'Top view of a tray of cracked eggs showing different levels of doneness' },
-  ];
-
-  const imageGrid = images.map(img => {
-    const src = require(`../../assets/gallery/${img.name}`);
+  // Generate image grid
+  const imageGrid = data.images.map(grid => {
+    const src = require(`../../assets/gallery/${grid.image}`);
     
     return `
       <div class="gallery__image">
-        <img src="${src}" alt="${img.alt}">
+        <img src="${src}" alt="${grid.alt}">
       </div>
     `;
   }).join('');
@@ -35,12 +29,12 @@ export function createGallery() {
       <div class="gallery__images">${imageGrid}</div>
 
       <div class="gallery__content">
-        <h2 class="gallery__title">What does cooking mean?</h2>
-        <p class="gallery__description">Is it simply applying heat to a food product? A way of making certain food safe to eat? Or a way to create flavour and make food more appealing? This is just part of what Hervé This, the father of molecular gastronomy, has dedicated his life to finding out. We spoke to him to find out what his experiments have told him. And in the process even discovered the secret to cooking the perfect egg...</p>
+        <h2 class="gallery__title">${data.title}</h2>
+        <p class="gallery__description">${data.description}</p>
 
         <div class="gallery__feature">
-          <h3 class="gallery__feature-title">The perfect egg</h3>
-          <p class="gallery__feature-description">Keep water between 67 and 68°C for a flavourful, tender yolk</p>
+          <h3 class="gallery__feature-title">${data.featureTitle}</h3>
+          <p class="gallery__feature-description">${data.featureDescription}</p>
         </div>
       </div>
     </div>
