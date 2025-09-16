ee69454dc3085a6b63ba3410bf69d0bd74b203b0
Tassha Nakagawa <nakagawatassha@gmail.com>
2025-09-16 22:31:25 +0800

    build: remove publicPath from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,6 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: devMode ? '/' : '/orchard-tasteology/',
   },
   plugins: [
     new MiniCssExtractPlugin({
